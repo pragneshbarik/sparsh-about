81f60c6274d3ed1ea113ea5ee6695a1d01a00c9a
pragneshbarik <barikpragnesh@gmail.com>
2023-03-02 00:48:52 +0530

    tried adding lazy loading.

--- src/Components/Parallax.js
+++ src/Components/Parallax.js
@@ -19,52 +19,68 @@ import {
 
 const layers = [layer1, layer2, layer3, layer4, layer5, layer6];
 
-function ParallaxImage(isMobile) {
-  return (src, index) => {
-    return (
-      <MouseParallaxChild
-        factorX={0.03 * (index + 1)}
-        factorY={0.03 * (index + 1)}
-      >
-        <motion.img
-          animate={{
-            y: [150 * index, -50],
-            opacity: [0, 1],
-            scale: isMobile ? 2.2 : 1.5,
-          }}
-          transition={{ ease: "easeInOut", type: "spring", duration: 3 }}
-          src={src}
-          style={{
-            position: "absolute",
-            bottom: 0,
-            transform: "scale(1.2)",
-            width: "100vw",
-          }}
-          alt="mountain"
-        />
-      </MouseParallaxChild>
-    );
-  };
-}
-
-function CinematicText(text) {
-  const n = text.length;
-  const letters = text.split("");
-  const cinematic_text = letters.map((letter, i) => {
-    return (
-      <motion.div
-        animate={{ x: [50 * (i - n / 2), 0], opacity: [0, 0.6] }}
-        transition={{ ease: "backOut", duration: 2.5, delay: 1.5 }}
-        style={{ display: "inline-block" }}
-      >
-        {letter}
-      </motion.div>
-    );
-  });
-  return cinematic_text;
-}
-
 export default function Parallax() {
+  const storedState = localStorage.getItem("loaded");
+  console.log(!!storedState);
+  const [loadedImages, setLoadedImages] = useState(0);
+  const [loaded, setLoaded] = useState(!!storedState);
+
+  function handleLoad() {
+    setLoadedImages((prev) => prev + 1);
+    console.log(loaded);
+  }
+
+  useEffect(() => {
+    if (loadedImages > 6) {
+      setLoaded(true);
+      localStorage.setItem("loaded", JSON.stringify(true));
+    }
+  }, [loadedImages]);
+
+  function CinematicText(text) {
+    const n = text.length;
+    const letters = text.split("");
+    const cinematic_text = letters.map((letter, i) => {
+      return (
+        <motion.div
+          animate={{ x: [50 * (i - n / 2), 0], opacity: [0, 0.6] }}
+          transition={{ ease: "backOut", duration: 2.5, delay: 1.5 }}
+          style={{ display: "inline-block" }}
+        >
+          {letter}
+        </motion.div>
+      );
+    });
+    return cinematic_text;
+  }
+  function ParallaxImage(isMobile) {
+    return (src, index) => {
+      return (
+        <MouseParallaxChild
+          factorX={0.03 * (index + 1)}
+          factorY={0.03 * (index + 1)}
+        >
+          <motion.img
+            animate={{
+              y: [150 * index, -50],
+              opacity: [0, 1],
+              scale: isMobile ? 2.2 : 1.5,
+            }}
+            transition={{ ease: "easeInOut", type: "spring", duration: 3 }}
+            src={src}
+            style={{
+              position: "absolute",
+              bottom: 0,
+              transform: "scale(1.2)",
+              width: "100vw",
+            }}
+            alt="mountain"
+          />
+        </MouseParallaxChild>
+      );
+    };
+  }
+
   const [width, setWidth] = useState(window.innerWidth);
 
   function handleWindowSizeChange() {
@@ -80,54 +96,95 @@ export default function Parallax() {
   const isMobile = width <= 768;
 
   return (
-    <MouseParallaxContainer
-      globalFactorX={0.3}
-      globalFactorY={0.3}
-      containerStyle={{
-        position: "relative",
-      }}
-      resetOnLeave
-    >
-      <MouseParallaxChild factorX={0.03} factorY={0.03}>
-        <img
-          src={bg}
-          style={{
-            transform: "scale(1.1)",
-            width: isMobile ? "fit-content" : "100vw",
-            height: "99.5vh",
-            opacity: "100%",
+    <>
+      <img src={bg} style={{ display: "none" }} onLoad={handleLoad} alt="a" />
+      <img
+        src={layer1}
+        style={{ display: "none" }}
+        onLoad={handleLoad}
+        alt="a"
+      />
+      <img
+        src={layer2}
+        style={{ display: "none" }}
+        onLoad={handleLoad}
+        alt="a"
+      />
+      <img
+        src={layer3}
+        style={{ display: "none" }}
+        onLoad={handleLoad}
+        alt="a"
+      />
+      <img
+        src={layer4}
+        style={{ display: "none" }}
+        onLoad={handleLoad}
+        alt="a"
+      />
+      <img
+        src={layer5}
+        style={{ display: "none" }}
+        onLoad={handleLoad}
+        alt="a"
+      />
+      <img
+        src={layer6}
+        style={{ display: "none" }}
+        onLoad={handleLoad}
+        alt="a"
+      />
+      {loaded && (
+        <MouseParallaxContainer
+          globalFactorX={0.3}
+          globalFactorY={0.3}
+          containerStyle={{
+            position: "relative",
           }}
-          alt="mountain"
-        />
-      </MouseParallaxChild>
-
-      {layers.map(ParallaxImage(isMobile))}
-
-      <MouseParallaxChild factorX={0.03 * 10} factorY={0.03 * 10}>
-        <motion.div
-          animate={{ y: [-50, 0] }}
-          transition={{ ease: "easeInOut", duration: 2, delay: 1 }}
-          style={styles.parallax_text_block}
+          resetOnLeave
         >
-          <motion.div
-            style={{
-              fontSize: isMobile ? 170 / 2 : 170,
-              fontFamily: "RavensaraSansMedium",
-              marginBottom: -40,
-            }}
-          >
-            {CinematicText("AURORA")}
-          </motion.div>
-          <motion.div
-            style={{ fontSize: isMobile ? 50 / 2 : 50, marginTop: 10 }}
-          >
-            {CinematicText("-de-")}
-          </motion.div>
-          <motion.div style={{ fontSize: isMobile ? 100 / 2 : 100 }}>
-            {CinematicText("FANTASIA")}
-          </motion.div>
-        </motion.div>
-      </MouseParallaxChild>
-    </MouseParallaxContainer>
+          <MouseParallaxChild factorX={0.03} factorY={0.03}>
+            <img
+              src={bg}
+              style={{
+                transform: "scale(1.1)",
+                width: isMobile ? "fit-content" : "100vw",
+                height: "99.5vh",
+                opacity: "100%",
+              }}
+              alt="mountain"
+            />
+          </MouseParallaxChild>
+
+          {layers.map(ParallaxImage(isMobile))}
+
+          <MouseParallaxChild factorX={0.03 * 10} factorY={0.03 * 10}>
+            <motion.div
+              animate={{ y: [-50, 0] }}
+              transition={{ ease: "easeInOut", duration: 2, delay: 1 }}
+              style={styles.parallax_text_block}
+            >
+              <motion.div
+                style={{
+                  fontSize: isMobile ? 170 / 2 : 170,
+                  fontFamily: "RavensaraSansMedium",
+                  marginBottom: -40,
+                }}
+              >
+                {CinematicText("AURORA")}
+              </motion.div>
+              <motion.div
+                style={{ fontSize: isMobile ? 50 / 2 : 50, marginTop: 10 }}
+              >
+                {CinematicText("-de-")}
+              </motion.div>
+              <motion.div style={{ fontSize: isMobile ? 100 / 2 : 100 }}>
+                {CinematicText("FANTASIA")}
+              </motion.div>
+            </motion.div>
+          </MouseParallaxChild>
+        </MouseParallaxContainer>
+      )}
+    </>
   );
 }
